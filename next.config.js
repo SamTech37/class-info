7ccdfca04fdb738491fb35cc22b0bc6f81e28096
SamTech37 <87892685+SamTech37@users.noreply.github.com>
2023-11-18 00:02:47 +0800

    removeConsole when building

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  compiler: {
+    removeConsole: true,
+    //removeConsole.log in production
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
