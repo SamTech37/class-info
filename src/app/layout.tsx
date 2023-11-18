1c415d911033d7730753d1f1aeb4991c9f9db76d
SamTech37 <87892685+SamTech37@users.noreply.github.com>
2023-11-18 14:30:49 +0800

    Refactor layout and navbar components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,78 @@
-// Import styles of packages that you've installed.
-// All packages except `@mantine/hooks` require styles imports
+//Root layout and navbar for the app
+"use client";
+
 import "@mantine/core/styles.css";
-
 import { MantineProvider, ColorSchemeScript } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { AppShell, Burger, Group, ScrollArea, Skeleton } from "@mantine/core";
 
-export const metadata = {
-  title: "My Mantine app",
-  description: "I have followed setup instructions carefully",
-};
+// export const metadata = {
+//   title: "NTHUCCC",
+//   description: "A better NTHU Course Catalog Client experience",
+// };
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
+  const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(true);
+
   return (
-    <html lang="en">
+    <html lang="zh">
       <head>
         <ColorSchemeScript />
+        <title>NTHUCCC</title>
+        <meta
+          name="description"
+          content="A better NTHU Course Catalog Client experience"
+        />
       </head>
       <body>
-        <MantineProvider defaultColorScheme="dark">{children}</MantineProvider>
+        <MantineProvider defaultColorScheme="dark">
+          <AppShell
+            header={{ height: 60 }}
+            navbar={{
+              width: 300,
+              breakpoint: "sm",
+              collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
+            }}
+            padding="md"
+          >
+            <AppShell.Header>
+              <Group h="100%" px="md">
+                <Burger
+                  opened={mobileOpened}
+                  onClick={toggleMobile}
+                  hiddenFrom="sm"
+                  size="sm"
+                />
+                <Burger
+                  opened={desktopOpened}
+                  onClick={toggleDesktop}
+                  visibleFrom="sm"
+                  size="sm"
+                />
+                NTHUCCC
+              </Group>
+            </AppShell.Header>
+            <AppShell.Navbar p="md">
+              <AppShell.Section>Side Navigation</AppShell.Section>
+              <AppShell.Section grow my="md" component={ScrollArea}>
+                {Array(60)
+                  .fill(0)
+                  .map((_, index) => (
+                    <Skeleton key={index} h={28} mt="sm" animate={false} />
+                  ))}
+              </AppShell.Section>
+              <AppShell.Section>
+                This site is NOT endorsed by NTHU
+              </AppShell.Section>
+            </AppShell.Navbar>
+            <AppShell.Main>{children}</AppShell.Main>
+          </AppShell>
+        </MantineProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import { useDisclosure } from "@mantine/hooks";
-import { AppShell, Burger, Group, ScrollArea } from "@mantine/core";
+import { ScrollArea } from "@mantine/core";
 import jsonData from "../Components/Departments.json";
 
 export default function Home() {
@@ -9,29 +9,15 @@ export default function Home() {
   const departments = jsonData;
 
   return (
-    <AppShell
-      header={{ height: 60 }}
-      navbar={{ width: 300, breakpoint: "sm", collapsed: { mobile: !opened } }}
-      padding="md"
-    >
-      <AppShell.Header>
-        <Group h="100%" px="md">
-          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
-          NTHU Course Info
-        </Group>
-      </AppShell.Header>
-      <AppShell.Navbar p="md">
-        <AppShell.Section>Colleges</AppShell.Section>
-        <AppShell.Section grow my="md" component={ScrollArea}>
-          {departments.map((dep, index) => (
-            <Link key={index} href={`/${dep}`}>
-              <p>{dep}</p>
-            </Link>
-          ))}
-        </AppShell.Section>
-        <AppShell.Section>This site is NOT endorsed by NTHU</AppShell.Section>
-      </AppShell.Navbar>
-      <AppShell.Main>Welcome</AppShell.Main>
-    </AppShell>
+    <ScrollArea h={500} type="always">
+      Welcome
+      <div>
+        {departments.map((dep, index) => (
+          <Link key={index} href={`/${dep}`}>
+            <p>{dep}</p>
+          </Link>
+        ))}
+      </div>
+    </ScrollArea>
   );
 }
