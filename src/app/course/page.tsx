5455f254034242cd817f30a5fa4519d59401604e
SamTech37 <SamTech37@users.noreply.github.com>
2024-06-03 01:06:12 +0800

    refactor: Add loading overlay to search form, update search result UI

--- src/app/course/page.tsx
+++ src/app/course/page.tsx
@@ -3,11 +3,12 @@
 "use client";
 
 import { Title } from "@mantine/core";
-import { TextInput, Button, Group, Box, NativeSelect } from "@mantine/core";
+import { TextInput, Button, Group, Box, LoadingOverlay } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 import { useRouter } from "next/navigation";
 
 import { useForm } from "@mantine/form";
-import { defaultSemester, availableSemesters, referenceLinks } from "@/config";
+import { referenceLinks } from "@/config";
 import Link from "next/link";
 export default function Home() {
   return (
@@ -19,16 +20,18 @@ export default function Home() {
 }
 
 function CourseIDForm() {
+  const [isLoading, { toggle }] = useDisclosure(false);
+
   const router = useRouter();
   const handleSubmit = () => {
-    const { courseID, semester } = courseIDForm.values;
-    let newPath = `/course/${semester}${courseID}`;
+    toggle(); // show loading overlay
+    const { courseID } = courseIDForm.values;
+    let newPath = `/course/${courseID}`;
     router.push(newPath);
   };
 
   const initialValues = {
     courseID: "",
-    semester: defaultSemester,
   };
 
   const courseIDForm = useForm({
@@ -37,16 +40,16 @@ function CourseIDForm() {
   });
 
   return (
-    <Box maw={400} mx="auto">
+    <Box maw={400} mx="auto" pos="relative">
+      <LoadingOverlay
+        visible={isLoading}
+        zIndex={1000}
+        overlayProps={{ radius: "xs", blur: 2 }}
+      />
       <form onSubmit={courseIDForm.onSubmit(() => handleSubmit())}>
-        <NativeSelect
-          label="學期"
-          data={availableSemesters}
-          {...courseIDForm.getInputProps("semester")}
-        />
         <TextInput
           label="科號"
-          description="不用輸入學期。如：CS135601"
+          description="如：11220CS135601 或 11310CS135501"
           {...courseIDForm.getInputProps("courseID")}
         />
 
